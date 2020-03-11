bb67b77cc8cc956187bd1cc51c10bb24cb839bf7
Ulugbek123 <31240653+Ulugbek123@users.noreply.github.com>
2020-03-11 13:15:18 +0000

    Update HashChain.py

--- HashChain.py
+++ HashChain.py
@@ -9,7 +9,7 @@ Challenge = "c89aa2ffb9edcc6604005196b5f0e0e4"
 #Prints the HASHES
 print(MD5HASH)
 
-#Loops the hashes until it reaches the original seed hash (Program knows orignal hash of "ecsc" due to import hashlib Which contains millions of hashes
+#Loops the hashes until it reaches the original seed hash (Program knows orignal hash of "ecsc" (Or Your input seed) due to import hashlib Which contains millions of hashes
 while MD5HASH != Challenge:
     MD5HASH = hashlib.md5(MD5HASH.encode()).hexdigest()  # calculate next hash
     print(MD5HASH)
