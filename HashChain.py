1a0426c381eb938845adeb099763253e1ab211d3
Ulugbek123 <31240653+Ulugbek123@users.noreply.github.com>
2020-03-11 13:07:57 +0000

    Create HashChain.py

---
+++ HashChain.py
@@ -0,0 +1,18 @@
+import hashlib
+
+#Gets the seed from the user
+seed = input('Enter your word to hash: ')
+#MD5 Hash
+MD5HASH = hashlib.md5(seed.encode()).hexdigest()
+#Challenge HASH
+Challenge = "c89aa2ffb9edcc6604005196b5f0e0e4"
+#Prints the HASHES
+print(MD5HASH)
+
+#Loops the hashes until it reaches the original seed hash (Program knows orignal hash of "ecsc" due to import hashlib Which contains millions of hashes
+while MD5HASH != Challenge:
+    MD5HASH = hashlib.md5(MD5HASH.encode()).hexdigest()  # calculate next hash
+    print(MD5HASH)
+
+
+print("This is the hash chain for the your seed ")
